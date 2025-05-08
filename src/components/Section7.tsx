6cde390726f6f46a7446f57526e3504605e65b4e
priyashanand <tomriddlealbussirius@gmail.com>
2025-05-08 23:23:12 +0530

    check icon for pricing changed

--- src/components/Section7.tsx
+++ src/components/Section7.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 
 // Checkmark Icon SVG Component
-const CheckIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5 text-green-500" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className={className}>
-    <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
-  </svg>
+const CheckIcon: React.FC<{ className?: string }> = () => (
+  <img src="/assets/section7img1.png" alt="" className='h-6 pr-2'/>
 );
 
 interface PlanFeature {
